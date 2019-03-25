59acfa7747130ee70ccd1afd433dab945720d79c
str1m <q.st.stream@gmail.com>
2019-03-26 01:36:11 +0300

    Edited hello_world

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name;
+    cin << "Enter your name";
+    cout << "Hello, world from"<<name << endl;
+     return 0;
+}
